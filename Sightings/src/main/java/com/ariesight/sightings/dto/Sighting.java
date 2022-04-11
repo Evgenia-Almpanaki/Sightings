e95ea44f3f78559b83609cc887d3151315d38491
Evgenia Almpanaki <evgenia.almpanaki@gmail.com>
2022-04-11 13:35:17 +0100

    Update Sighting.java

--- Sightings/src/main/java/com/ariesight/sightings/dto/Sighting.java
+++ Sightings/src/main/java/com/ariesight/sightings/dto/Sighting.java
@@ -1,9 +1,11 @@
 package com.ariesight.sightings.dto;
 
 import com.ariesight.sightings.dto.Characters.SCharacter;
+import java.util.Date;
 
 public class Sighting {
     
     private SCharacter character;
     private Location location;
+    private Date date;
 }
