de1a02977e7ab6d104692ef05ab7200b2ac4207b
Evgenia Almpanaki <evgenia.almpanaki@gmail.com>
2022-04-11 13:34:03 +0100

    Creating Sighting DTO

---
+++ Sightings/src/main/java/com/ariesight/sightings/dto/Sighting.java
@@ -0,0 +1,9 @@
+package com.ariesight.sightings.dto;
+
+import com.ariesight.sightings.dto.Characters.SCharacter;
+
+public class Sighting {
+    
+    private SCharacter character;
+    private Location location;
+}
