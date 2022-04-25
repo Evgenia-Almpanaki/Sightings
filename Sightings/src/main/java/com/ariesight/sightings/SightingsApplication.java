ceca1a386acbcc6d37607946d5bc0468b9ccf3fc
Evgenia Almpanaki <evgenia.almpanaki@gmail.com>
2022-04-25 11:40:13 +0100

    Added comments [NOT IMPORTANT]

--- Sightings/src/main/java/com/ariesight/sightings/SightingsApplication.java
+++ Sightings/src/main/java/com/ariesight/sightings/SightingsApplication.java
@@ -1,13 +1,22 @@
+/**
+ * Superhero/Villain registration site application
+ */
 package com.ariesight.sightings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class
+ * It triggers the Spring Boot Application.
+ *
+ * @author Evgenia Almpanaki
+ */
 @SpringBootApplication
 public class SightingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SightingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SightingsApplication.class, args);
+    }
 
 }
